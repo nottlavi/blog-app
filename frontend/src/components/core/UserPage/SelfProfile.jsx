8ec9aa7a3adda7320874f75b8ad56deecbc00874
Lavi <laviarora4234@gmail.com>
2025-09-26 14:12:04 +0530

    all set to launch request for updating profile / frontend done

--- frontend/src/components/core/UserPage/SelfProfile.jsx
+++ frontend/src/components/core/UserPage/SelfProfile.jsx
@@ -27,6 +27,12 @@ export const SelfProfile = () => {
   //looading imp info from redux, this profile will be used to update the details
   const profile = useSelector((state) => state.auth.profile);
 
+  const [email, setEmail] = useState(profile.email);
+  const [firstName, setFirstName] = useState(profile.firstName);
+  const [lastName, setLastName] = useState(profile.lastName);
+  const [password, setPassword] = useState("");
+
+
   //need to decide whether to keep this effect or not, for now i wont keep it
   // useEffect(() => {
   //   const getProfile = async () => {
@@ -58,21 +64,60 @@ export const SelfProfile = () => {
           <ModalBody>
             <form className="flex flex-col">
               <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" />
+              <input type="email" id="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
 
               <label htmlFor="firstName">First Name:</label>
-              <input type="text" id="firstName" name="firstName" />
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+              />
 
               <label htmlFor="lastName">Last Name:</label>
-              <input type="text" id="lastName" name="lastName" />
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
 
               <label htmlFor="password">Password:</label>
-              <input type="password" id="password" name="password" />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
 
-              <img src={preview || profile.profilePic} />
+              <img
+                src={preview || profile.profilePic}
+                className="rounded-full w-32 h-32"
+              />
               <button type="button" onClick={() => fileRef.current.click()}>
                 change avatar
               </button>
+              {preview ? (
+                <button
+                  onClick={() => {
+                    setPreview(null);
+                  }}
+                  type="button"
+                >
+                  delete selected profile pic
+                </button>
+              ) : (
+                <div></div>
+              )}
               <input
                 type="file"
                 hidden
