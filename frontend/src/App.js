97a2763ce3f894ac32d939d17dce684c1dc8aaf6
Lavi <laviarora4234@gmail.com>
2025-09-26 13:51:17 +0530

    add ui to update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ import UpdateProfilePage from "./pages/UpdateProfilePage";
 
 function App() {
   const dispatch = useDispatch();
-  const profile = useSelector((state) => state.auth.profile);
+  const token = useSelector((state) => state.auth.token);
   useEffect(() => {
     //checked it, the token is valid and im getting it
     const token = localStorage.getItem("token");
@@ -70,10 +70,6 @@ function App() {
           <Route path="/blog/:blogId" element={<BlogPage />} />
           <Route path="/user/:userId" element={<UserPage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route
-            path="/update-profile"
-            element={profile ? <UpdateProfilePage /> : <LogInPage />}
-          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
--- frontend/src/components/core/UserPage/SelfProfile.jsx
+++ frontend/src/components/core/UserPage/SelfProfile.jsx
@@ -1,13 +1,97 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
 
 export const SelfProfile = () => {
-  //looading imp info from redux
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const fileRef = useRef(null);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [preview, setPreview] = useState(null);
+
+  // //this will be be used to display info of the current profile being viewed by some other user, no to decide whether to keep this part
+  // const {userId} = useParams();
+  // console.log(userId);
+
+  //looading imp info from redux, this profile will be used to update the details
   const profile = useSelector((state) => state.auth.profile);
 
-  return <div className="card p-6">
-    hello {profile.firstName}
-    <button >update profile</button>
-  </div>;
+  //need to decide whether to keep this effect or not, for now i wont keep it
+  // useEffect(() => {
+  //   const getProfile = async () => {
+  //     try {
+  //       const res = await axios.post(
+  //         `${BASE_URL}/user/profile`,
+  //         {
+  //           userId: profile._id,
+  //         },
+  //         { withCredentials: true }
+  //       );
+  //       // console.log(res)
+  //     } catch (err) {
+  //       console.log(err.message);
+  //     }
+  //   };
+  //   getProfile();
+  // }, [profile]);
 
+  return (
+    <div className=" p-6 flex flex-col">
+      hello {profile.firstName}
+      <Button onClick={onOpen}>Update Profile</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form className="flex flex-col">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+
+              <label htmlFor="firstName">First Name:</label>
+              <input type="text" id="firstName" name="firstName" />
+
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" id="lastName" name="lastName" />
+
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" />
+
+              <img src={preview || profile.profilePic} />
+              <button type="button" onClick={() => fileRef.current.click()}>
+                change avatar
+              </button>
+              <input
+                type="file"
+                hidden
+                ref={fileRef}
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  if (file) {
+                    setPreview(URL.createObjectURL(file));
+                  }
+                }}
+              ></input>
+            </form>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant="ghost">Save</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
 };
